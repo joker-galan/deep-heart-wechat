3fea0d767b667760bb6a391c09f4608eb55e6ac8
XueYuan <jkr.galan@gmail.com>
2019-06-10 23:26:25 +0800

    create requert

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,64 +1,45 @@
 // pages/home/home.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
-    data: {},
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    data: {
+        calendars: [],
+        weeks: ['日', '一', '二', '三', '四', '五', '六'],
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
+    onLoad: function (options) {
+        var self = this.data;
+        wx.request({
+            url: 'http://127.0.0.1:7777/calendar/info', //仅为示例，并非真实的接口地址,
+            method: 'POST',
+            header: {
+                'content-type': 'application/json' // 默认值
+            },
+            success(res) {
+                if (res.data.code === '1') {
+                    console.log(res.data)
+                    self.calendars = res.data.obj.days || [];
+                }
+                console.log(self.calendars)
+            }
+        })
+    },
     onReady: function () {
 
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
     onShow: function () {
 
     },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
     onHide: function () {
 
     },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
     onUnload: function () {
 
     },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
     onPullDownRefresh: function () {
 
     },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
     onReachBottom: function () {
 
     },
-
-    /**
-     * 用户点击右上角分享
-     */
     onShareAppMessage: function () {
 
     }
-})
+});
