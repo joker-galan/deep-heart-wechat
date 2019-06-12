89321bdc4bcb3a801adb9158a8bd2b8f84215818
XueYuan <jkr.galan@gmail.com>
2019-06-12 17:37:25 +0800

    码了二丢丢！

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,25 +1,36 @@
 // pages/home/home.js
 Page({
     data: {
+        offset: 0,
         calendars: [],
         weeks: ['日', '一', '二', '三', '四', '五', '六'],
+        touchItem: 'calendar-day-item',
+        curItem: 12
     },
     onLoad: function (options) {
         var self = this;
         wx.request({
-            url: 'http://127.0.0.1:7777/calendar/info', //仅为示例，并非真实的接口地址,
+            url: 'http://122.114.191.222:7777/calendar/info', //仅为示例，并非真实的接口地址,
             method: 'POST',
             header: {
                 'content-type': 'application/json' // 默认值
             },
             success(res) {
                 if (res.data.code === '1') {
+                    console.log(res.data);
                     self.setData({
+                        offset: res.data.obj.beginWeek % 7,
                         calendars: (res.data.obj.days || [])
                     })
                 }
             }
         })
+    },
+    touchStart: function (event) {
+        this.setData({touchItem: event.currentTarget.dataset.key});
+    },
+    touchEnd: function () {
+        this.setData({touchItem: 0});
     },
     onReady: function () {
 
